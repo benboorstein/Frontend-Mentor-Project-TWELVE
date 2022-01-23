49250a9472f16ff7631e7ee272d994d0d4c1e0b5
Ben <benboorstein@gmail.com>
2022-01-23 11:18:51 -0500

    initial commit for Frontend Mentor project twelve

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+/* From FEM:
+Your users should be able to:
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers (details provided in the README.md)
+*/
